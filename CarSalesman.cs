ad19096f2c84674ee7d255e166d7eb3011dd1b8c
lexvieira <lexvieira.info@gmail.com>
2020-04-08 23:02:35 +0700

    Method Required

--- CarSalesman.cs
+++ CarSalesman.cs
@@ -7,6 +7,10 @@ namespace OOP
         public CarSalesman(string firstName, string lastName) : base(firstName, lastName)
         {
         }
-
+        public override void Sell() //Override is used only with Abstract Classes
+        {
+            //Method is required, needs to have its own definition
+            Console.WriteLine(String.Format("HI my name is {0}. I would recommend you to buy this car", this.FullName));
+        }
     }
 }
