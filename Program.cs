35cad9e9ac2604575ad8921ea18b0dd7574979d0
lexvieira <lexvieira.info@gmail.com>
2020-04-08 19:56:53 +0700

    First Commit OOP_Udemy

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarSalesman steveTheSalesMan = new CarSalesman("Steve", "Rogers");
+            Console.WriteLine(steveTheSalesMan.FullName);
+            steveTheSalesMan.Sell();
+
+            RetailSalesPerson erikTheSalesDude = new RetailSalesPerson("Erik","Erickson");
+            Console.WriteLine(erikTheSalesDude.FullName);
+            erikTheSalesDude.Sell();
+        }
+    }
+}
