e2f9bb51d12129e0f381fc3c922d148570504b30
lexvieira <lexvieira.info@gmail.com>
2020-04-08 21:13:10 +0700

    Abstract Class Implementation

--- CarSalesman.cs
+++ CarSalesman.cs
@@ -2,25 +2,15 @@ using System;
 
 namespace OOP
 {
-    public class CarSalesman
+    public class CarSalesman : Salesman
     {
-        private string _firstName;
-
-        private string _lastname;
-
-        public string FullName{
-            get {
-                return string.Format("{0} {1}", this._firstName, this._lastname);
-            }
-        }
-        public CarSalesman(string firstName, string lastName){
-            this._firstName = firstName;
-            this._lastname = lastName;
-        }
-
-        public void Sell()
+        public CarSalesman(string firstName, string lastName) : base(firstName, lastName)
         {
-            Console.WriteLine(string.Format("Hi, my name is {0}. I would recommend you to buy this card", this.FullName));
+        }
+
+        public override void Sell()
+        {
+            Console.WriteLine(String.Format("HI my name is {0}. I would recommend you to buy this car", this.FullName));
         }
     }
 }
--- readme.txt
+++ readme.txt
@@ -13,7 +13,19 @@ git pull OOP_Udemy master:fstcm
 
 git pull OOP_Udemy fstcm
 
-
 SOLUTION
 https://github.community/t5/How-to-use-Git-and-GitHub/How-to-deal-with-quot-refusing-to-merge-unrelated-histories-quot/m-p/16305#
 git pull OOP_Udemy master:fstcm --allow-unrelated-histories
+
+
+>> ABSTRACT CLASS 
+https://www.udemy.com/course/beginner-object-oriented-programming-in-csharp-and-dotnet-core/learn/lecture/10716604#notes
+
+>>>> 
+One quick thing you might be wondering about is what happens when you have more parameters for the child
+class that might not be going into a base class.
+And the answer is we just don't pass it into the base class.
+We set those in the child class.
+An example would be H So if we have age or anything else in this case I just mentioned age.
+So if we say in age and this doesn't really go anywhere but we needed for our child class then we would
+>>>>
